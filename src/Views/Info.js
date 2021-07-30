2e75b6106729d723d2568a6f09d5bec8c7e7c385
Tabatha Young <tyoung@newrelic.com>
2021-07-30 12:32:14 -0700

    chore: add update timestamp to footer

--- src/Views/Info.js
+++ src/Views/Info.js
@@ -11,12 +11,12 @@ function Backstory(){
             <h2>The Schedule</h2>
             <h3>We have the cabin for a few days, but on Saturday and Sunday, there is no solid schedule. We can do this however we want! We do ask that you communicate to us your plans closer to the trip. Are you taking a day trip to Seattle or a National Park? Should we do something as a group? Feel free to share ideas. </h3>
             <h2>The Menu</h2>
-            <h3>One important reason we ask about plans is so we can come up with a menu for the weekend. We don't mind cooking dinners and maybe grabbing some sandwich fixins for everyone, but a headcount would be very important. We plan to make a big trip to a local grocery store on our way in. Please feel free to grab any snacks or drinks for yourself that you would want. Reach out if you have any dietary concerns we should know about. Richie sould be able to procure a pony keg from 10 Barrel. The cabin has a full kitchen and a grill. If by chance someone has a menu request or wants to cook a meal, please let us know! Otherwise, we are winging it!</h3>
+            <h3>One important reason we ask about plans is so we can come up with a menu for the weekend. We don't mind cooking dinners and maybe grabbing some sandwich fixins for everyone, but a headcount would be very important. We plan to make a big trip to a local grocery store on our way in. Please feel free to grab any snacks or drinks for yourself that you would want. Reach out if you have any dietary concerns we should know about. Richie should be able to procure a pony keg from 10 Barrel. The cabin has a full kitchen and a grill. If by chance someone has a menu request or wants to cook a meal, please let us know! Otherwise, we are winging it!</h3>
             <h3>On the evening after the ceremony, Greg is making carnitas tacos! So that plan is taken care of. If someone cannot eat pork, please let us know. </h3>
             <h2>Photographs</h2>
             <h3>The priority within our budget was the photographer. This day is about making memories, and the photos are the living reminders. The ceremony day might feel slightly different than a normal wedding as we work with our photographer to capture our story. He will be more involved and part of the party. He has a method that we trust, and we can give you more info that weekend so you know what to expect. I would also like to note that our contract with the photographer prohibits personal photos during the ceremony. Someone using a cellphone camera is not a very appealing look in a photo, and we are paying him to make sure our photos are beautiful. There will be lots of opportunites for selfies, and everyone will have access to the photo gallery when it's ready. </h3>
             <br/><br/>
-            Built and Designed by Tabatha Delane ©2021 
+            Built and Designed by Tabatha Delane ©2021    •    Last Updated: Fri Jul 30, 2021
 
         </div>
     )}
